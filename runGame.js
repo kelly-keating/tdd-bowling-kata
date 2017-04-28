53054288a9769c98bd3edb066db6a25d5e2375b7
kelly-keating <keating.kel@gmail.com>
2017-04-28 17:20:14 +1200

    interval working

--- runGame.js
+++ runGame.js
@@ -2,15 +2,16 @@ var generateBoard = require('./generateBoard.js')
 var getNext = require('./getNextGeneration.js')
 
 function runGame(size){
-  var board = generateBoard(size)
+  generateBoard(size)
 
-  var interval = setInterval(getNext, 200)
+  var interval = setInterval(function (){
+    generateBoard(size)
+  }, 200)
 
   setTimeout(function() {
     clearInterval(interval)
   }, 30000)
 }
-
 
 runGame(20)
 
