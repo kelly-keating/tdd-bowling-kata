907a305929fbd6ff03524fc28ce63399a924db7e
kelly-keating <keating.kel@gmail.com>
2017-04-28 17:05:15 +1200

    end of day - interval added

--- runGame.js
+++ runGame.js
@@ -2,15 +2,15 @@ var generateBoard = require('./generateBoard.js')
 var getNext = require('./getNextGeneration.js')
 
 function runGame(size){
-  //board = make first board
   var board = generateBoard(size)
-  // loop through:
-  for(var generations = 0; generations < 100; generations++){
-    //board = get next board
-    board = getNext(board)
-    //display result
-  }
+
+  var interval = setInterval(getNext, 200)
+
+  setTimeout(function() {
+    clearInterval(interval)
+  }, 30000)
 }
+
 
 runGame(20)
 
